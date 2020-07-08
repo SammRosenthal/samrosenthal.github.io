10e792abfec0052d493db4bfb5dc6b048aeabd0b
Sam Rosenthal <samrrosenthal@gmail.com>
2020-07-07 20:27:20 -0700

    skill section initial version done

--- src/components/tech/Tech.js
+++ src/components/tech/Tech.js
@@ -4,8 +4,35 @@ import "./tech.css";
 const Tech = () => {
   return (
     <div className="techContainer">
-      <h2>Skills</h2>
-      <div className="skillsContainer"></div>
+      <h2 className="title">Skills</h2>
+      <div className="skillsContainer">
+        <div className="skills">
+          <h3>Languages</h3>
+          <ul>
+            <li>JavaScript</li>
+            <li>Java</li>
+            <li>Python</li>
+          </ul>
+        </div>
+
+        <div className="skills">
+          <h3>Frameworks</h3>
+          <ul>
+            <li>React</li>
+            <li>Struts2</li>
+            <li>Spring Boot</li>
+          </ul>
+        </div>
+
+        <div className="skills">
+          <h3>Tools</h3>
+          <ul>
+            <li>Git</li>
+            <li>Jira/Confluence</li>
+            <li>Windows/Mac/Debian OS</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
