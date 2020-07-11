5399515d0cd3b8d64ba6ae894fdd302d1f7234ee
Sam Rosenthal <samrrosenthal@gmail.com>
2020-07-11 10:21:32 -0700

    added more css and structure to project portion of app

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,7 +4,8 @@ import "./projects.css";
 
 const Projects = () => {
   return (
-    <div>
+    <div className="parentProjectContainer">
+      <Project />
       <Project />
       <Project />
       <Project />
--- src/components/projects/project/Project.js
+++ src/components/projects/project/Project.js
@@ -3,7 +3,15 @@ import "./project.css";
 
 const Project = () => {
   return (
-    <div>Placeholder of a project that will be in the Projects component</div>
+    <div className="projectContainer">
+      <h1>Project Title</h1>
+      <a>Project Link</a>
+      <p id="description">
+        Placeholder of a project that will be in the Projects component
+      </p>
+      <br />
+      <p id="tech">placeholder tech</p>
+    </div>
   );
 };
 
