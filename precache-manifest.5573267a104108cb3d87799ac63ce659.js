7fa6ba04681287ce4226c2a29db1b1b0636eae59
Sam Rosenthal <samrrosenthal@gmail.com>
2020-07-20 08:47:13 -0700

    Updates

---
+++ precache-manifest.5573267a104108cb3d87799ac63ce659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa4db2cc11571567fb7f90625de6332",
+    "url": "/samrosenthal/index.html"
+  },
+  {
+    "revision": "695891d1697848736167",
+    "url": "/samrosenthal/static/css/main.574af0fa.chunk.css"
+  },
+  {
+    "revision": "512e761caa85fdcc75c9",
+    "url": "/samrosenthal/static/js/2.09331fe7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/samrosenthal/static/js/2.09331fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695891d1697848736167",
+    "url": "/samrosenthal/static/js/main.278bf346.chunk.js"
+  },
+  {
+    "revision": "4f0ae8fe8f4623d47ff2",
+    "url": "/samrosenthal/static/js/runtime-main.2fac039c.js"
+  }
+]);
